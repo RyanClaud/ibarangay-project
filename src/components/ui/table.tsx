718ea96e97818c40765a5f738ef62a58bb601633
Claud Ryan L. <ryanclaud4@gmail.com>
2025-11-01 20:34:57 +0000

    add border color to all table headers and also the search box

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -73,7 +73,7 @@ const TableHead = React.forwardRef<
   <th
     ref={ref}
     className={cn(
-      "h-12 px-4 text-left align-middle font-semibold text-muted-foreground bg-muted/50 [&:has([role=checkbox])]:pr-0",
+      "h-12 border-b border-t px-4 text-left align-middle font-semibold text-muted-foreground bg-muted/50 [&:has([role=checkbox])]:pr-0",
       className
     )}
     {...props}
